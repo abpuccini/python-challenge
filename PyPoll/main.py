5dc8d42ab4edaaf2f861b03be8e0149589570ede
abpuccini <b.atcharaporn@gmail.com>
2020-09-17 16:45:05 -0400

    PyPoll Winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,4 @@
-# PyPoll Analysis
+# PyPoll
 
 ## Implementing modules 
 
@@ -9,13 +9,15 @@ import os
 
 datafile = os.path.join('Resources', 'election_data.csv')
 
-## Analysis
+## Election Results
 
 ### Part I: Reading file, Storing header and Initializing sets
 
 candidate_votes = {}                                        # Dictionary {Candidate: Votes}
-candidate = []                                              # List of all votes
-candidate_name = []                                         # List of candidate names               
+election_votes = []                                         # List of all votes
+candidate_name = []                                         # List of election_votes names               
+percent_votes = []                                          # List of percentage votes for each election_votes
+winner = []
 
 with open(datafile, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')  
@@ -23,49 +25,35 @@ with open(datafile, 'r') as csvfile:
     for rows in csvreader:
         name_index = 2                                      # Index of names in rows is [2]
         name = rows[name_index]
-        candidate.append(name)                              # Adding name to the list
-    for n in candidate:                                     # n represents each element in candidate list
-        if n not in candidate_name:                         # Removing duplicated candidate names
+        election_votes.append(name)                         # Adding name to the list
+    for n in election_votes:                                # n represents each element in election_votes list
+        if n not in candidate_name:                         # Removing duplicated election_votes names
             candidate_name.append(n)                        # List of candidates
-    for i in candidate_name:                                # i represents each element in candidate_name list 
-        candidate_votes[i] = candidate.count(i)             # Adding key and value into candidate_vote dictionary
-        total = sum(candidate_votes.values())               # Sum of all values in dictionary
-        
-        
+
+### Part II: Total votes
+
+for i in candidate_name:                                    # i represents each element in candidate_name list 
+    candidate_votes[i] = election_votes.count(i)            # Adding key and value into candidate_vote dictionary
+
+total = sum(candidate_votes.values())                       # Sum of all values in dictionary
+
+### Part III: Number and Percentage of Each candidates' votes
+
+candidate_votes_list = list(candidate_votes.values())        # Creating vote count list
+
+for p in candidate_votes_list:     
+    p_votes = round(float(p / total * 100), 2)               # % of votes for each election_votes
+    percent_votes.append("%.3f" % p_votes)                   # Adding % of votes with 3 decimals to the list
+    
+### Part IV: The Winner
+
+for m in candidate_votes_list:
+    if m == max(candidate_votes_list):
+        name = candidate_name[candidate_votes_list.index(m)]    # Identify winner by 
+
+print(name)
 
 
 
 
 
-print(total)
-
-
-        
-        
-# with open(datafile, 'r') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')  
-#     header = next(csvreader)
-#     for row in csvreader:
-#         name_index = 2
-#         candidate_name = row[name_index]                    # List of Candidate names
-#         if candidate_name in candidate_vote.keys():
-#             candidate_vote[candidate_name] += 1
-#         else:
-#             candidate_vote[candidate_name] = 1
-
-# # {'Khan': 1}
-
-# # total votes
-# totalvotes = sum(candidate_vote.values())
-# print(f'Total votes {totalvotes}')
-# # calculate inv % candidate
-# percent = []
-# for i in candidate_vote:
-#     percent = round(float(candidate_vote[i] / totalvotes) * 100 , 0)
-#     print(f'{i} : {percent}% : {candidate_vote[i]}')
-# # finding candidate with max votes
-# for key in candidate_vote.keys():
-#     if candidate_vote[key] == max(candidate_vote.values()):
-#         winner = key
-
-# print(f'Winner is: {winner}')
