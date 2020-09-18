261bd90e177bf2ea87235c5c05f78f4f9910a431
abpuccini <b.atcharaporn@gmail.com>
2020-09-18 16:52:26 -0400

    PyPoll Completed Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,16 @@
 # PyPoll
 
-## Implementing modules 
+# Part I: Implementing modules and Analysis 
 
 import csv
 import os
 
-## File path
+# File path
 
 data_filepath = os.path.join('Resources', 'election_data.csv')
 analysis_filepath = os.path.join('Analysis', 'PyPoll-analysis.txt')
 
-## Election Results
-
-### Part I: Reading file, Storing header and Initializing sets
+# Part II: Reading file, Storing header and Initializing sets
 
 candidate_votes = {}                                        # Dictionary {Candidate: Votes}
 election_votes = []                                         # List of all votes
@@ -31,14 +29,14 @@ with open(data_filepath, 'r') as csvfile:
         if n not in candidate_name:                         # Removing duplicated election_votes names
             candidate_name.append(n)                        # List of candidates
 
-### Part II: Total votes
+# Part III: Total votes
 
 for i in candidate_name:                                    # i represents each element in candidate_name list 
     candidate_votes[i] = election_votes.count(i)            # Adding key and value into candidate_vote dictionary
 
 total = sum(candidate_votes.values())                       # Sum of all values in dictionary
 
-### Part III: Number and Percentage of Each candidates' votes
+# Part IV: Number and Percentage of Each candidates' votes
 
 vote_counts = list(candidate_votes.values())        # Creating vote count list
 
@@ -46,21 +44,21 @@ for p in vote_counts:
     p_votes = round(float(p / total * 100), 2)               # % of votes for each election_votes
     percent_votes.append("%.3f" % p_votes)                   # Adding % of votes with 3 decimals to the list
     
-### Part IV: The Winner
+# Part V: The Winner
 
-#### Identify winner by using index of the maximum votes because they are arranged in the same order
+# Identify winner by using index of the maximum votes because they are arranged in the same order
 
 for m in vote_counts:
     if m == max(vote_counts):
         winner = candidate_name[vote_counts.index(m)]
 
-### Part V: Election Result and exporting result to txt.file
-
-#### Creating votes_chart list containing name, %votes and vote counts
+# Part VI: Creating votes_chart list containing name, %votes and vote counts
 
 for i in range(len(candidate_name)):
     votes_chart = f'{candidate_name[i]}: {percent_votes[i]}% ({vote_counts[i]})'
     chart.append(votes_chart)
+
+# Part VII: Election Result and exporting result to txt.file
 
 result = [
     'Election Results',
