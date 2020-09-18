1ec2c1ba7a2c8588588931c96bca8c26ea8be563
abpuccini <b.atcharaporn@gmail.com>
2020-09-18 16:47:39 -0400

    PyBank Completed Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,18 @@
 # PyBank
 
-# Implementing modules 
+# Part I: Implementing Modules and defining file path 
 
 import csv
 import os
 
-## File path
+# File path
 
 data_filepath = os.path.join('Resources', 'budget_data.csv')
 output_filepath = os.path.join('Analysis', 'output.csv')
 analysis_filepath = os.path.join('Analysis', 'PyBank-analysis.txt')
 
-## Financial Analysis
 
-### Part I: Reading file, Storing header and Initializing sets
+# Part II: Reading file, Storing header and Initializing sets
 
 headers = []                                        # set of data header
 rows = []                                           # set of data 
@@ -38,31 +37,31 @@ with open(data_filepath, 'r') as csvfile:
             delta = pl_list[x] - pl_list[x-1]       # delta is profit/losses differences between period x and x-1
         delta_list.append(int(delta))               # List of changes in profit/losses
         
-### Part II: The total number of months included in the dataset
+# Part III: The total number of months included in the dataset
 
 month = len(period)                       
         
-### Part III: The net total amount of "Profit/Losses" over the entire period
+# Part IV: The net total amount of "Profit/Losses" over the entire period
 
 total = sum(pl_list)                        
 
-### Part IV: The average of the changes in "Profit/Losses" over the entire period        
+# Part V: The average of the changes in "Profit/Losses" over the entire period        
      
 delta_sum = sum(delta_list)
 delta_no = len(delta_list) - 1                      # Number of data in delta_list which is not included index[0] 
 diff_avg = round(float(delta_sum / delta_no), 2)
 
-### Part V: The greatest increase in profits (date and amount) over the entire period
+# Part VI: The greatest increase in profits (date and amount) over the entire period
 
 Max = max(delta_list)
 max_period = period[delta_list.index(Max)]
 
-### Part VI: The greatest decrease in losses (date and amount) over the entire period 
+# Part VII: The greatest decrease in losses (date and amount) over the entire period 
 
 Min = min(delta_list)
 min_period = period[delta_list.index(Min)]
 
-### Part VII: Storing additional data into CSV file
+# Part VIII: Storing additional data into CSV file
 
 output = zip(period, pl_list, delta_list)
 output_headers = headers + ['Diff']
@@ -72,7 +71,7 @@ with open(output_filepath, 'w') as datafile:
     writer.writerow(output_headers)
     writer.writerows(output)
 
-### Part VIII: Analysis Result, Print to Terminal and Exporting result to txt.file
+# Part IX: Analysis Result, Print to Terminal and Exporting result to txt.file
 
 result = [
     'Financial Analysis ',
